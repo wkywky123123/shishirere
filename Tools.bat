fccf5fc511f4526c0280311028d95e708de35644
wkywky123123 <89382167+wkywky123123@users.noreply.github.com>
2023-08-14 15:53:28 +0800

    Update Tools.bat

--- Tools.bat
+++ Tools.bat
@@ -1,22 +1,3 @@
-@ECHO OFF
-REM  QBFC Project Options Begin
-REM  HasVersionInfo: No
-REM Companyname: 
-REM Productname: 
-REM Filedescription: 
-REM Copyrights: 
-REM Trademarks: 
-REM Originalname: 
-REM Comments: 
-REM Productversion:  0. 0. 0. 0
-REM Fileversion:  0. 0. 0. 0
-REM Internalname: 
-REM ExeType: console
-REM Architecture: x64
-REM Appicon: 
-REM AdministratorManifest: No
-REM  QBFC Project Options End
-@ECHO ON
 @echo off
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
