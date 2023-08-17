b56dc690f79b8344ab97deaf59b80b9617f26395
wkywky123123 <89382167+wkywky123123@users.noreply.github.com>
2023-08-17 08:58:16 +0800

    修复重复添加的问题

--- Tools.bat
+++ Tools.bat
@@ -1,17 +1,48 @@
 @echo off
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-echo 正在请求管理员权限...
-goto UACPrompt
-) else ( goto gotAdmin )
+    echo 请求管理员权限...
+    goto UACPrompt
+) else (
+    goto gotAdmin
+)
+
 :UACPrompt
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 "%temp%\getadmin.vbs"
 exit /B
+
 :gotAdmin
- 
-echo 正在修改hosts文件...
+echo 修改或添加域名解析记录...
+
 cd C:\Windows\System32\drivers\etc
-echo  cd C:\Windows\System32\drivers\etc
-echo  172.67.193.224 www.shireyishunjian.com>>hosts
+
+set "domain=www.shireyishunjian.com"
+set "ip=172.67.193.224"
+set "hostsFile=hosts"
+
+REM Check if the domain exists in the hosts file
+findstr /C:"%domain%" "%hostsFile%" >nul
+
+if %errorlevel% equ 0 (
+    echo 域名已存在，正在更新解析记录...
+    REM Update the existing domain's IP address
+    (for /f "tokens=*" %%a in ('type "%hostsFile%" ^& break ^> "%hostsFile%"') do (
+        echo %%a | findstr /C:"%domain%" >nul
+        if %errorlevel% equ 0 (
+            echo %ip% %domain%
+        ) else (
+            echo %%a
+        )
+    )) > "%hostsFile%.tmp"
+    move /y "%hostsFile%.tmp" "%hostsFile%"
+    echo 解析记录已更新。
+) else (
+    echo 域名不存在，正在添加解析记录...
+    REM Append the new domain's IP address
+    echo %ip% %domain% >> "%hostsFile%"
+    echo 解析记录已添加。
+)
+
+pause
